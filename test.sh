73471c1a970ecb4f1da7c1bdbdfdd8d5349dde56
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-03-06 17:50:24 +0100

    fixed script is in face of spaces in paths and filenames

--- test.sh
+++ test.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-PROG=$1
-DIR=$2
+PROG="$1"
+DIR="$2"
 RED='\033[1;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-FILES=$DIR/*.in
+FILES="$DIR"/*.in
 
 for f in $FILES
 do
-out=${f::-2}
+out="${f::-2}"
 out="${out}out"
 
-err=${f::-2}
+err="${f::-2}"
 err="${err}err"
-valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all -q ./$PROG -g <$f >$PROG.out 2>$PROG.err
+valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all -q ./"$PROG" -g <"$f" >"$PROG".out 2>"$PROG".err
 
-
-
-DIFF1=$(diff $out $PROG.out)
-DIFF2=$(diff $err $PROG.err)
+DIFF1="$(diff "$out" "$PROG".out)"
+DIFF2="$(diff "$err" "$PROG".err)"
 if [ "$DIFF1" ] || [ "$DIFF2" ]; then
 	echo -e "${RED} ${f} didn't pass ${NC}"
 else
@@ -26,5 +24,5 @@ else
 fi
 
 done
-rm $PROG.out
-rm $PROG.err
+rm "$PROG".out
+rm "$PROG".err
